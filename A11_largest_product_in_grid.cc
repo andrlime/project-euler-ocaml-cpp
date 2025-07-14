4539eed74c1aa073d62a96ce85b46ee1c4413071
Andrew Li <andrewli@u.northwestern.edu>
2025-07-14 02:36:34 -0500

    add solution to 11

---
+++ A11_largest_product_in_grid.cc
@@ -0,0 +1,84 @@
+#include "shared.hpp"
+
+constexpr size_t grid_size = 20;
+constexpr size_t adjacent_count = 4;
+
+enum Direction {
+    Up = 0,
+    Down,
+    Left,
+    Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+};
+
+const int64_t
+solve(const std::vector<std::vector<int>>& grid)
+{
+    std::array<std::array<int64_t, grid_size>, grid_size> products{};
+    for (int i = 0; i < grid_size; i++)
+        products[i].fill(0);
+
+    std::array<int64_t, 8> arr{};
+    for (int i = 0; i < grid_size; i++) {
+        for (int j = 0; j < grid_size; j++) {
+            arr.fill(1);
+            for (int k = 0; k < adjacent_count; k++) {
+                if (j - k >= 0) [[likely]] arr[Direction::Up] *= grid[i][j - k];
+                else arr[Direction::Up] = 0;
+
+                if (j + k < grid_size) [[likely]] arr[Direction::Down] *= grid[i][j + k];
+                else arr[Direction::Down] = 0;
+
+                if (i - k >= 0) [[likely]] arr[Direction::Left] *= grid[i - k][j];
+                else arr[Direction::Left] = 0;
+
+                if (i + k < grid_size) [[likely]] arr[Direction::Right] *= grid[i + k][j];
+                else arr[Direction::Right] = 0;
+
+                if (i - k >= 0 && j - k >= 0) [[likely]] arr[Direction::UpLeft] *= grid[i - k][j - k];
+                else arr[Direction::UpLeft] = 0;
+
+                if (i + k < grid_size && j - k >= 0) [[likely]] arr[Direction::UpRight] *= grid[i + k][j - k];
+                else arr[Direction::UpRight] = 0;
+
+                if (i - k >= 0 && j + k < grid_size) [[likely]] arr[Direction::DownLeft] *= grid[i - k][j + k];
+                else arr[Direction::DownLeft] = 0;
+
+                if (i + k < grid_size && j + k < grid_size) [[likely]] arr[Direction::DownRight] *= grid[i + k][j + k];
+                else arr[Direction::DownRight] = 0;
+            }
+            products[i][j] = std::ranges::max(arr);
+        }
+    }
+
+    
+
+    return std::ranges::max(products | std::views::join);
+}
+
+int
+main()
+{
+    std::vector<std::vector<int>> input;
+    std::ifstream file("input_0011.txt");
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<std::string> tokens = split(line, ' ');
+
+        std::vector<int> numbers;
+        for (std::string s : tokens) {
+            numbers.push_back(stoi(s));
+        }
+
+        input.push_back(numbers);
+    }
+
+    solver(solve, input);
+
+    file.close();
+    return 0;
+}
--- A59_xor_decryption.cc
+++ A59_xor_decryption.cc
@@ -1,21 +1,27 @@
 #include "shared.hpp"
 
 const bool
-decrypt(std::string &s, const std::vector<char>& cipher, const std::array<char, 3> key) {
-    for (std::size_t index = 0; index < cipher.size(); index ++) {
+decrypt(std::string& s, const std::vector<char>& cipher, const std::array<char, 3> key)
+{
+    for (std::size_t index = 0; index < cipher.size(); index++) {
         const char next_char = cipher[index] ^ (key[index % 3]);
 
         const bool is_lowercase = (next_char >= 'a' && next_char <= 'z');
         const bool is_uppercase = (next_char >= 'A' && next_char <= 'Z');
         const bool is_digit = (next_char >= '0' && next_char <= '9');
         const bool is_space = next_char == ' ';
-        const bool is_reasonable_punctuation = next_char == ',' || next_char == '.' || next_char == ';' || next_char == ':';
-        const bool is_other_punctuation = next_char == '!' || next_char == '-' || next_char == '?' || next_char == '\'' || next_char == '\"';
+        const bool is_reasonable_punctuation = next_char == ',' || next_char == '.'
+                                               || next_char == ';' || next_char == ':';
+        const bool is_other_punctuation = next_char == '!' || next_char == '-'
+                                          || next_char == '?' || next_char == '\''
+                                          || next_char == '\"';
         const bool is_parenthesis = next_char == '(' || next_char == ')';
         const bool is_bracket = next_char == '[' || next_char == ']';
         const bool is_math = next_char == '+' || next_char == '/';
 
-        if (is_lowercase || is_uppercase || is_digit || is_space || is_reasonable_punctuation || is_other_punctuation || is_parenthesis || is_bracket || is_math) {
+        if (is_lowercase || is_uppercase || is_digit || is_space
+            || is_reasonable_punctuation || is_other_punctuation || is_parenthesis
+            || is_bracket || is_math) {
             s.push_back(next_char);
             continue;
         }
@@ -32,12 +38,12 @@ solve(const std::vector<char>& cipher)
     std::array<char, 3> key{'a', 'a', 'a'};
     std::string s;
 
-    for (std::size_t i = 0; i < 26; i ++) {
-        for (std::size_t j = 0; j < 26; j ++) {
-            for (std::size_t k = 0; k < 26; k ++) {
+    for (std::size_t i = 0; i < 26; i++) {
+        for (std::size_t j = 0; j < 26; j++) {
+            for (std::size_t k = 0; k < 26; k++) {
                 s.clear();
                 if (!decrypt(s, cipher, key)) {
-                    key[2] ++;
+                    key[2]++;
                     continue;
                 }
 
@@ -48,15 +54,15 @@ solve(const std::vector<char>& cipher)
                 return sum;
             }
 
-            key[1] ++;
+            key[1]++;
             key[2] = 'a';
         }
 
-        key[0] ++;
+        key[0]++;
         key[1] = 'a';
         key[2] = 'a';
     }
-    
+
     return -1;
 }
 
@@ -68,7 +74,7 @@ main()
 
     std::string line;
     std::getline(file, line);
-    
+
     std::vector<std::string> tokens = split(line, ',');
 
     std::vector<char> cipher;
